b559ddd968ade9f7866868e84161f518c64a9a2f
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-18 11:16:55 -0600

    webpack setup, package.json updated, still doing initial setup

--- client/src/components/Shop.jsx
+++ client/src/components/Shop.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Shop = (props) => {
+const Shop = props => {
+  let modPrice = Number(props.price) * Math.random() + Number(props.price);
+  modPrice = modPrice.toFixed(2);
 
-    let modPrice = (Number(props.price) * Math.random()) + Number(props.price);
-    modPrice = modPrice.toFixed(2);
-
-    return (
-        <div className={'seller-K'}>
-            <div className={'other-price-row'}>
-                <div id={'other-price'} className={'red-K'}>${modPrice.toString()}</div>
-                <button id={'other-add-cart'}>Add to Cart</button>
-            </div>
-            <div className={'other-product-details'}>+ Free Shipping
-                Sold by: <a href="#">{props.shop.name}</a></div>
+  return (
+    <div className={"seller-K"}>
+      <div className={"other-price-row"}>
+        <div id={"other-price"} className={"red-K"}>
+          ${modPrice.toString()}
         </div>
-    )
-
+        <button id={"other-add-cart"}>Add to Cart</button>
+      </div>
+      <div className={"other-product-details"}>
+        + Free Shipping Sold by: <a href="#">{props.shop.name}</a>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
