ba213f4919cd898e5c55c597efcac9b8a350aa03
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-18 14:53:13 -0600

    further init setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
-var express = require('express');
-var cors = require('cors');
-var path = require('path');
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
 var app = express();
 
 app.use(cors());
 
+app.get("/", (req, res) => {});
 
-app.use(express.static('./client/dist/'));
+app.use(express.static("./client/dist/"));
 
-
-app.listen(3380, function () {
-    console.log('listening on port 3380!');
-});
+app.listen(3380, console.log("listening on port 3380!"));
