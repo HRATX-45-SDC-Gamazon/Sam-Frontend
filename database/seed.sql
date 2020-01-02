a7af6eae4a9e4f408a1abc582e3800cd39fdb684
Sam Lawson <slawson355@gmail.com>
2020-01-02 14:46:55 -0600

    deployed psql db, it is now all on aws ec2

--- database/seed.sql
+++ database/seed.sql
@@ -47,9 +47,24 @@
 -- psql
 CREATE TABLE IF NOT EXISTS sdcGammazon (
   id SERIAL PRIMARY KEY,
-  primaryKey INT = ,
+  primaryKey INT,
   "name" TEXT,
   price TEXT,
   stock INT,
   "group" TEXT
 );
+
+-- COPY sdcgammazon (primaryKey, "name", price, stock, "group")
+-- FROM '/usr/csvs/2dataToSeed.csv'
+-- DELIMITER ',' csv header;
+
+
+-- scp -i "[full path to PEM file]" "[a csv full path]" ec2-user@ec2-52-15-181-244.us-east-2.compute.amazonaws.com:~/csvs
+
+
+-- scp -i "/Users/samlawson/Desktop/SDC/key19.pem" "/Users/samlawson/Desktop/SDC/seedData/dataToSeedFirst.csv" ec2-user@ec2-52-15-181-244.us-east-2.compute.amazonaws.com:~/csvs
+-- scp -i "/Users/samlawson/Desktop/SDC/key19.pem" "/Users/samlawson/Desktop/SDC/seedData/2dataToSeed.csv" ec2-user@ec2-52-15-181-244.us-east-2.compute.amazonaws.com:~/csvs
+-- scp -i "/Users/samlawson/Desktop/SDC/key19.pem" "/Users/samlawson/Desktop/SDC/seedData/2dataToSeed.csv" "/Users/samlawson/Desktop/SDC/seedData/3dataToSeed.csv" ec2-user@ec2-52-15-181-244.us-east-2.compute.amazonaws.com:~/csvs
+
+
+-- ssh -i "/Users/samlawson/Desktop/SDC/key19.pem" ec2-user@ec2-52-15-181-244.us-east-2.compute.amazonaws.com
